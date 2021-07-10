5d0394a009707ee90fa90e37be092821089e070f
gokul <gokulakams@gmail.com>
2021-07-10 16:49:22 +0530

    swallow patch added

--- config.h
+++ config.h
@@ -6,20 +6,58 @@ static const unsigned int snap      = 32;       /* snap pixel */ static const in
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FiraCode Nerd Font:size=12" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#2e3440";
-static const char col_gray2[]       = "#3b4252";
-static const char col_gray3[]       = "#d8dee9";
-static const char col_gray4[]       = "#e5e9f0";
-static const char col_cyan[]        = "#5e81ac";
-static const char col_black[]       = "#2e3440";
-static const char col_red[]         = "#bf616a";
-static const char col_yellow[]      = "#ebcb8b";
-static const char col_white[]       = "#d8dee9";
+// static const char col_gray1[]       = "#2e3440";
+// static const char col_gray2[]       = "#3b4252";
+// static const char col_gray3[]       = "#d8dee9";
+// static const char col_gray4[]       = "#e5e9f0";
+// static const char col_cyan[]        = "#5e81ac";
+// static const char col_black[]       = "#2e3440";
+// static const char col_red[]         = "#bf616a";
+// static const char col_yellow[]      = "#ebcb8b";
+// static const char col_white[]       = "#d8dee9";
+// #define G_dark0         #282828
+// #define G_dark1         #3c3836
+// #define G_dark2         #504945
+// #define G_dark3         #665c54
+// #define G_dark4         #7c6f64
 
+// #define G_medium        #928374
+
+// #define G_light0        #fdf4c1
+// #define G_light1        #ebdbb2
+// #define G_light2        #d5c4a1
+// #define G_light3        #bdae93
+// #define G_light4        #a89984
+
+// #define G_red0          #fb4934
+// #define G_green0        #b8bb26
+// #define G_yellow0       #fabd2f
+// #define G_blue0         #83a598
+// #define G_purple0       #d3869b
+// #define G_aqua0         #8ec07c
+// #define G_orange0       #fe8019
+
+// #define G_red1          #9d0006
+// #define G_green1        #79740e
+// #define G_yellow1       #b57614
+// #define G_blue1         #076678
+// #define G_purple1       #8f3f71
+// #define G_aqua1         #427b58
+// #define G_orange1       #af3a03
+
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#3c3836";
+static const char col_gray3[]       =  "#504945";
+static const char col_gray4[]       = "#928374";
+static const char col_cyan[]        = "#fdf4c1";
+static const char col_black[]       = "#2e3440";
+static const char col_red[]         = "#9d0006";
+static const char col_yellow[]      = "#fabd2f";
+static const char col_white[]       = "#d8dee9";
 static const char *colors[][3]      = {
 	/*					fg         bg          border   */
-	[SchemeNorm] =	 { col_cyan, col_gray1,  col_gray2 },
-	[SchemeSel]  =	 { col_gray4, col_gray1,   col_cyan },
+	[SchemeNorm] =	 { col_gray4, col_gray1,  col_gray2 },
+	[SchemeSel]  =	 { col_cyan, col_gray1,   col_cyan },
 	[SchemeWarn] =	 { col_black, col_yellow, col_red },
 	[SchemeUrgent]=	 { col_white, col_red,    col_red },
 };
@@ -32,9 +70,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       2,            0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           2,         0,          0,          -1,        -1 },
+	{ "kitty",      NULL,     NULL,        0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -66,7 +106,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *browsercmd[] = {"firefox",NULL};
+static const char *browsercmd[] = {"firefox","brave-browser",NULL};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
@@ -115,6 +155,7 @@ static Button buttons[] = {
 	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
 	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
 	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
+	{ ClkStatusText,        0,              Button4,        sigstatusbar,   {.i = 4} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
